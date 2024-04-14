339e17dd8df7992fa3d904147d78cbe89a97bf8f
vplotnikov16 <vplotnikov16@yandex.ru>
2024-04-14 20:33:30 +0500

    Изменен тип аргумента circle, передаваемого в конструктор CCircle, теперь QPoint. Первая созданная окружность сразу выделяется. Выделение окружности по ЛКМ. Если же координата мыши при нажатии ЛКМ попала в пересечение нескольких окружностей, то выделяется ближайшая окружность

--- rendering.py
+++ rendering.py
@@ -4,10 +4,16 @@ from PyQt5.QtCore import Qt, QPoint
 
 
 class CCircle:
-    def __init__(self, center: tuple[int, int], radius: float):
-        self.center = QPoint(center[0], center[1])
+    def __init__(self, center: QPoint, radius: float):
+        self.center = center
         self.radius = radius
         self.selected = False
+
+    def is_point_inside(self, point: QPoint) -> bool:
+        return self.squared_dist_to(point) <= self.radius ** 2
+
+    def squared_dist_to(self, point: QPoint) -> int:
+        return (point.x() - self.center.x()) ** 2 + (point.y() - self.center.y()) ** 2
 
 
 def draw_circle(circle: CCircle, qp: QPainter):
@@ -23,10 +29,12 @@ def draw_circle(circle: CCircle, qp: QPainter):
 class Canvas(QWidget):
     def __init__(self, parent: QWidget | None = None):
         super(Canvas, self).__init__(parent=parent)
-        self.circle_container = []
+        self.circle_container: list[CCircle] = []
 
     def add_circle(self, event: QMouseEvent):
-        circle = CCircle(center=(event.x(), event.y()), radius=40)
+        circle = CCircle(center=event.pos(), radius=40)
+        if not self.circle_container:
+            circle.selected = True
         self.circle_container.append(circle)
 
     def paintEvent(self, event: QPaintEvent):
@@ -42,6 +50,27 @@ class Canvas(QWidget):
         self.circle_container.clear()
         self.update()
 
+    def intersections(self, point: QPoint) -> list[CCircle]:
+        result = []
+        for circle in self.circle_container:
+            if circle.is_point_inside(point):
+                result.append(circle)
+        return result
+
     def mousePressEvent(self, event: QMouseEvent):
-        self.add_circle(event)
+        intersections = self.intersections(event.pos())
+        if intersections:
+            nearest_circle = intersections.pop()
+            while intersections:
+                current_circle = intersections.pop()
+                if current_circle.squared_dist_to(event.pos()) < nearest_circle.squared_dist_to(event.pos()):
+                    nearest_circle = current_circle
+            nearest_circle.selected = True
+            for circle in self.circle_container:
+                if circle is nearest_circle:
+                    continue
+                if circle.selected:
+                    circle.selected = False
+        else:
+            self.add_circle(event)
         self.update()
